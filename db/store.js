0991d5285a2759acf563fe5a23a73036dd61ea51
Dave <diacovmusic@gmail.com>
2021-03-08 14:35:46 -0500

    commit with some errors for inspection

--- db/store.js
+++ db/store.js
@@ -1,35 +1,38 @@
-const util = require('util');
+const util = require("util");
+const fs = require("fs");
 
 //This package will be used to generate a unique id
-const uuidv1 = require('uuid')
+const uuidv1 = require("uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-
-
-
 //create our class
 class Store {
-    read(){
-        return readFileAsync('db/db.json', 'utf8')
-    }
+  read() { 
+    console.log("checking ...");
+    return readFileAsync(path.join(__dirname, "db.json"), "utf8");
+  }
 
-    write(note) {
-        return writeFileAsync('db/db.json', JSON.stringify(note));
-    }
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
 
-    //creat a function to get notes
-
-    //create a function to add notes
-
-    //create a function to remove notes BY ID -- YOU CANNOT DO THIS WITHOUT GETTING UUID TO WORK
-
-
-
-
-
+  //creat a function to get notes
+  async getNotes() {
+    const noteList = JSON.parse(await this.read());
+    console.log(noteList);
+    return noteList;
+  }
+  //create a function to add notes
+  writeNotes(note) {
+    let noteList = JSON.parse(read());
+    noteList.push(note);
+    this.write(noteList);
+    console.log(noteList);
+  }
+  //create a function to remove notes BY ID -- YOU CANNOT DO THIS WITHOUT GETTING UUID TO WORK
+  deleteNotes() {}
 }
 
 //export new store
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,33 @@
 //import items needed
 const router = require("express").Router();
-const store = require("../db/store")
+const store = require("../db/store");
 
 //make a GET request with all the notes from the database
 
-router.get('/notes', (req, res) => {
-    store
+router.get("/notes", (req, res) => {
+  store
     .getNotes()
     .then((notes) => {
-        return res.json(notes);
+        console.log(notes);
+      return res.json(notes);
     })
-    .catch((err) => res.status(500).json(err))
-
-
-
-})
+    .catch((err) => res.status(500).json(err));
+});
 
 //create a post request
+router.post("/notes", (req, res) => {
+    store.writeNotes(req.body);
+})
+
 
 //create a delete request
+router.delete("/notes/:id", (req, res) => {
+  store
+    .deleteNotes(req.params.id)
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
 
 module.exports = router;
